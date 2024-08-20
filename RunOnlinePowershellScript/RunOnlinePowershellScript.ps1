4e7240d9c6fe096e1c92ccfb4daa33bf86656c26
Anders <atl@pck.dk>
2024-08-20 22:55:51 +0200

    Update RunOnlinePowershellScript.ps1

--- RunOnlinePowershellScript/RunOnlinePowershellScript.ps1
+++ RunOnlinePowershellScript/RunOnlinePowershellScript.ps1
@@ -7,16 +7,18 @@ try
 catch 
 {
     clear-host
+    sleep 1
     write-host ""
-    write-host "                                                                                           " -BackGroundColor Black
-    write-host " ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR " -ForeGroundColor DarkRed -BackGroundColor Black
-    write-host "                                                                                           " -BackGroundColor Black
-    write-host "                  An error was detected while running the online installer                 " -ForeGroundColor Yellow -BackGroundColor Black
-    write-host "                  The installer was not able to self-remediate the error                   " -ForeGroundColor Yellow -BackGroundColor Black
-    write-host "                                                                                           " -BackGroundColor Black
-    write-host "       You may need to use the offline installer if the target system is unsupported       " -ForeGroundColor Yellow -BackGroundColor Black
-    write-host "                 Please forward the below error message to the developer                   " -ForeGroundColor Yellow -BackGroundColor Black
-    write-host "                                                                                           " -BackGroundColor Black
+    write-host "                                                                                            " -BackGroundColor Black
+    write-host " ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! " -ForeGroundColor DarkRed -BackGroundColor Black
+    write-host "                                                                                            " -BackGroundColor Black
+    write-host "                  An error was detected while running the online installer                  " -ForeGroundColor Yellow -BackGroundColor Black
+    write-host "         The installer was not able to self-remediate the error and was terminated          " -ForeGroundColor Yellow -BackGroundColor Black
+    write-host "                            Any changes made have been reverted                             " -ForeGroundColor Yellow -BackGroundColor Black
+    write-host "                                                                                            " -BackGroundColor Black
+    write-host "       You may need to use the offline installer if the target system is unsupported        " -ForeGroundColor Yellow -BackGroundColor Black
+    write-host "                 Please forward the below error message to the developer                    " -ForeGroundColor Yellow -BackGroundColor Black
+    write-host "                                                                                            " -BackGroundColor Black
     write-host ""
     write-host $_.Exception.Message -ForeGroundColor Red -BackGroundColor Black
     write-host ""
