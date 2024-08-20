99d25f5d8958c2ace407f345e5007f9d1be75f1a
Anders <atl@pck.dk>
2024-08-20 22:55:39 +0200

    Update OnlineDeploy_Windows-ManageIPv6.ps1

--- WindowsManageIPv6/OnlineDeploy_Windows-ManageIPv6.ps1
+++ WindowsManageIPv6/OnlineDeploy_Windows-ManageIPv6.ps1
@@ -1,23 +1,24 @@
 try 
 {
-    $key="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3Bja3RzL1dpbmRvd3MtTWFuYWdlSVB2Ni9tYWluL0RlcGxveV9XaW5kb3dzLU1hbmFnZUlQdjYucHMx"
+    $key="BASE64_ENCODED_URL"
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
     $l=[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($key));iex (iwr $l -UseBasicParsing).Content
 }
 catch 
 {
     clear-host
+    sleep 1
     write-host ""
-    write-host "                                                                                           " -BackGroundColor Black
-    write-host " ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR ERROR " -ForeGroundColor DarkRed -BackGroundColor Black
-    write-host "                                                                                           " -BackGroundColor Black
-    write-host "                  An error was detected while running the online installer                 " -ForeGroundColor Yellow -BackGroundColor Black
-    write-host "         The installer was not able to self-remediate the error and was terminated         " -ForeGroundColor Yellow -BackGroundColor Black
-    write-host "                            Any changes made have been reverted                            " -ForeGroundColor Yellow -BackGroundColor Black
-    write-host "                                                                                           " -BackGroundColor Black
-    write-host "       You may need to use the offline installer if the target system is unsupported       " -ForeGroundColor Yellow -BackGroundColor Black
-    write-host "                 Please forward the below error message to the developer                   " -ForeGroundColor Yellow -BackGroundColor Black
-    write-host "                                                                                           " -BackGroundColor Black
+    write-host "                                                                                            " -BackGroundColor Black
+    write-host " ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! ERROR! " -ForeGroundColor DarkRed -BackGroundColor Black
+    write-host "                                                                                            " -BackGroundColor Black
+    write-host "                  An error was detected while running the online installer                  " -ForeGroundColor Yellow -BackGroundColor Black
+    write-host "         The installer was not able to self-remediate the error and was terminated          " -ForeGroundColor Yellow -BackGroundColor Black
+    write-host "                            Any changes made have been reverted                             " -ForeGroundColor Yellow -BackGroundColor Black
+    write-host "                                                                                            " -BackGroundColor Black
+    write-host "       You may need to use the offline installer if the target system is unsupported        " -ForeGroundColor Yellow -BackGroundColor Black
+    write-host "                 Please forward the below error message to the developer                    " -ForeGroundColor Yellow -BackGroundColor Black
+    write-host "                                                                                            " -BackGroundColor Black
     write-host ""
     write-host $_.Exception.Message -ForeGroundColor Red -BackGroundColor Black
     write-host ""
